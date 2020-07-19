5a400b1d3b928af8ff9811d6abb6a3a14c22df64
Denis Mysenko <denis@mysenko.com>
2020-07-19 17:57:55 +1000

    r misc

--- src/Console/Commands/Format.php
+++ src/Console/Commands/Format.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class Format
+ * Class Format.
  */
 class Format extends Command
 {
