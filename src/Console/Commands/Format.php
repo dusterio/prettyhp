f07b18b3fba37566114d7971de15f8888f2c044c
Denis Mysenko <denis@mysenko.com>
2020-07-25 18:27:57 +1000

    r commit2

--- src/Console/Commands/Format.php
+++ src/Console/Commands/Format.php
@@ -37,6 +37,7 @@ class Format extends Command
         $this
             ->setName('format')
             ->setDescription('Formatt a PHP source code file')
+            ->setDescription('Format a PHP source codee file')
             ->setDefinition(
                 new \Symfony\Component\Console\Input\InputDefinition(array(
                     new \Symfony\Component\Console\Input\InputArgument('filename', \Symfony\Component\Console\Input\InputArgument::REQUIRED),
